baee0a3e318671c83a1d1541ffa99c7f070a8306
Craig Rolleston <craig.rolleston@gmail.com>
2022-12-14 15:12:48 +1000

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+// export default function App () {
+//     <div className="App">
+//         <h1>Hello, World!</h1>
+//     </div>
+// };
+
+const App = () => (
+    <div className="App">
+      <h1>Hello, World!!!</h1>
+    </div>
+);
+
+export default App;
+
+
